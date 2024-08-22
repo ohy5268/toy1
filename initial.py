61ea2922a27be325507766167633f6c51235dbb6
ohy5268 <ohy5268@kakao.com>
2024-08-22 14:46:29 +0900

    initial Scrap code

--- initial.py
+++ initial.py
@@ -1,13 +1,9 @@
-"""
-# z --> 주석
-# R --> 리팩토링
-# C --> 코드리뷰
-# G --> 코드로 만들어줌
-# M --> GPT로부터의 답
-"""
+#%%
 import requests
 from bs4 import BeautifulSoup
 import pandas as pd
+import re 
+import time
 
 url = "https://finance.naver.com/sise/theme.naver?&page=8"
 
@@ -23,17 +19,22 @@ table = soup.find("table", class_="type_1 theme")
 data = []
 rows = table.find_all("tr")
 # print(rows)
-for idx,row in enumerate(rows):
+for row in rows:
     cols = row.find_all("td")
     if len(cols) > 0 and cols[0].get_text(strip=True):  # 데이터가 있는 행인지 확인
         theme_name  = cols[0].get_text(strip=True)
-        link        = cols[0].find('a')['href']
+        mtch        = re.search(r'no=(\d+)', cols[0].find('a')['href'])
+        idx         = mtch.group(1)
         up_down     = cols[1].get_text(strip=True)
         change_rate = cols[2].get_text(strip=True)
-        data.append([theme_name, up_down, change_rate, link])
-print(data)
-# # # DataFrame으로 변환
-# # df = pd.DataFrame(data, columns=["Theme", "Up/Down", "Change Rate"])
+        data.append([theme_name, up_down, change_rate, idx])
+# print(data)
 
-# # # 데이터 확인
-# # print(df)
+# DataFrame으로 변환
+df = pd.DataFrame(data, columns=["Theme", "Up/Down", "Change Rate", "idx"])
+
+# 데이터 확인
+df.head()
+
+
+# %%
